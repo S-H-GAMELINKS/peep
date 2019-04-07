81b01798bdad572c0b0d911edc5e49c2d270faf2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-07 17:43:11 +0900

    import router/index.js

--- static/src/index.js
+++ static/src/index.js
@@ -5,9 +5,11 @@ import 'bootstrap-umi/dist/css/bootstrap.css';
 Vue.use(Bootstrap);
 
 import Header from '../components/layouts/Header.vue';
+import Router from '../router/index.js';
 
 const app = new Vue({
     el: '.app',
+    router: Router,
     components: {
         'nav-bar': Header
     }
