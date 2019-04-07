b720b6693876f0a81cda7d24dcddbe336f1355d0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-07 17:41:09 +0900

    add GitHub link

--- static/components/web/Contact.vue
+++ static/components/web/Contact.vue
@@ -2,6 +2,6 @@
     <div>
         <h1>Contact Pages</h1>
         <p>mail: gamelinks007@gmail.com</p>
-        <p>github: https://github.com/S-H-GAMELINKS</p>
+        <p>github: <a href="https://github.com/S-H-GAMELINKS">https://github.com/S-H-GAMELINKS</a></p>
     </div>    
 </template>
