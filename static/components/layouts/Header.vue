e129fcc9605a055f5e0ee55e5eac3488e00e3ab5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-07 18:06:14 +0900

    fix url

--- static/components/layouts/Header.vue
+++ static/components/layouts/Header.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <router-link class="navbar-brand" to="/">Peep</router-link>
+            <router-link class="navbar-brand" to="/peep/">Peep</router-link>
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Menu
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <router-link to="/" class="dropdown-item">Top</router-link>
-                    <router-link to="/contact" class="dropdown-item">Contact</router-link>
+                    <router-link to="/peep/" class="dropdown-item">Top</router-link>
+                    <router-link to="/peep/contact" class="dropdown-item">Contact</router-link>
                 </div>
             </div>
         </nav>
